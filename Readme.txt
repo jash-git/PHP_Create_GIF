018aca5fdcf33a31918658144d5076bffa2cb22b
Gitea <gitea@fake.local>
2019-08-02 10:04:08 +0800

    Create

---
+++ Readme.txt
@@ -0,0 +1,10 @@
+﻿PHP 建立 GIF
+
+資料來源: https://github.com/Sybio/GifCreator
+https://hant.helplib.com/GitHub/article_90780
+
+
+相關專業 線上建立GIF網站:
+    https://ezgif.com/
+    https://www.soogif.com/ 
+
---
+++ index.php
@@ -0,0 +1,25 @@
+﻿<?php
+include "GifCreator.php";
+$frames = array(
+'./images/frame_0_delay-0.6s.png',
+'./images/frame_1_delay-0.6s.png',
+'./images/frame_2_delay-0.6s.png',
+'./images/frame_3_delay-0.6s.png',
+'./images/frame_4_delay-0.6s.png',
+'./images/frame_5_delay-0.6s.png',
+'./images/frame_6_delay-0.6s.png',
+'./images/frame_7_delay-0.6s.png'
+);
+
+$durations = array(60, 60, 60, 60,60,60,60,60);//60ms
+
+$gc = new GifCreator();
+$gc->create($frames, $durations, 0);
+
+$gifBinary = $gc->getGif();
+
+file_put_contents('./animated_picture.gif', $gifBinary);
+
+echo 'finish';
+
+?>
